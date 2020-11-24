03f65b67283996782e057d487d7a456dfeb04ece
Florent Duboquet <44649273+FlorentDuboquet@users.noreply.github.com>
2020-11-24 14:24:28 +0100

    add lecture

--- utils.py
+++ utils.py
@@ -1,17 +1,24 @@
 
-def lecture (file_name) :
-    liste = []
+def lecture(file_name):
+    liste1= []
+    liste2= []
     with open(file_name) as f:
         ligne = f.readline()
         elem = ligne.split()
         results = map(int, elem)
         results = list(map(int, results))
         nbr = results[0]
-        for i in range (nbr) :
+        for i in range(nbr):
             ligne = f.readline()
             elem = ligne.split()
             results = map(int, elem)
             results = list(map(int, results))
-            liste.append(liste)
+            liste1.append(results)
+        for i in range(nbr):
+            ligne = f.readline()
+            elem = ligne.split()
+            results = map(int, elem)
+            results = list(map(int, results))
+            liste2.append(results)
 
-    return liste
+    return liste1, liste2,
